f882e92e2634f4c7ba47b21440c54c2a48ab124c
theAnunaki <the_anunaki@tutanota.com>
2023-07-17 17:58:49 -0700

    added 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int m = 122;
+
+	while (m >= 97)
+	{
+		putchar(m);
+		m--;
+	}
+	putchar('\n');
+	return (0);
+
+}
