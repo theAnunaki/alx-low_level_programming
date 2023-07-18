9a304da91faa0d8749683c10eb7a257ebdca4a30
theAnunaki <the_anunaki@tutanota.com>
2023-07-17 19:25:50 -0700

    12 rectification

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (digit1 = 0; digit1 <= 98; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 99; digit2++)
+		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
 		{
 			putchar((digit1 / 10) + '0');
 			putchar((digit1 % 10) + '0');
