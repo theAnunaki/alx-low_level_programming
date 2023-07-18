64222857eb19796ca47e72f02e469a25910a09fb
theAnunaki <the_anunaki@tutanota.com>
2023-07-17 17:27:19 -0700

    alpgabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int m = 97;
+	int n  = 65;
+
+	while (m <= 122)
+	{
+		putchar(m);
+		m++;
+	}
+	while (n <= 90)
+	{
+		putchar(n);
+		n++
+	}
+	putchar('\n');
+	return (0);
+
+}
