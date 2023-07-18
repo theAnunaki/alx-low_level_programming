88d7a8c6a5bee430d17d85a70515a5e896b70929
theAnunaki <the_anunaki@tutanota.com>
2023-07-17 17:50:52 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - using the printf functionto print a line
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = 0; k <= 9; k++)
+	{
+		putchar(k + 48);
+	}
+	putchar("\n");
+	return (0);
+}
