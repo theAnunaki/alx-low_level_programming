be19f112d8307bd44c6cf1baa5b4af4e13e58534
theAnunaki <the_anunaki@tutanota.com>
2023-07-17 18:04:10 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int m;
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(m);		
+	}
+	while (m = 97; m <=102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+
+}
